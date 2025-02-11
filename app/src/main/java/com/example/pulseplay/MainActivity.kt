cf3d72ddfbf4c4e48694de327e97fed0a0ef13ee
ButchNinoButas <butchbutas69@gmail.com>
2025-02-11 09:17:48 +0800

    activity UI

--- app/src/main/java/com/example/pulseplay/MainActivity.kt
+++ app/src/main/java/com/example/pulseplay/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.pulseplay
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,21 +14,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        // Simulate a login process (replace with your actual login logic)
-        val isLoginSuccessful = true // Assume login is successful
-        if (isLoginSuccessful) {
+
+        val loginButton = findViewById<Button>(R.id.btn_login)
+        val registerText = findViewById<TextView>(R.id.tv_register)
+        val intent = Intent(this@MainActivity, Register::class.java)
+        startActivity(intent)
+        finish()
+
+
+        loginButton.setOnClickListener {
             navigateToHomePage()
         }
+
+
     }
 
     private fun navigateToHomePage() {
-        val intent = Intent(this, HomePage::class.java)
+        val intent = Intent(this@MainActivity, HomePage::class.java)
         startActivity(intent)
-        finish() // Optional: Close MainActivity to prevent going back
+        finish() // Close MainActivity so the user can't go back to it
     }
+
 }
